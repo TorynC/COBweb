d488a0f102aea729def7b6b9ed37079318d52638
Toryn <torync03@gmail.com>
2025-01-18 13:16:53 -0500

    help

--- src/Sidebar/help.jsx
+++ src/Sidebar/help.jsx
@@ -12,8 +12,9 @@ const Help = ({ visible, onClose }) => {
                     <button className="help-popup-close" onClick={onClose}>
                         &times;
                     </button>
-                    <h2>Help/FAQ</h2>
-                    <p>Here you can provide some helpful information or FAQs.</p>
+                    <h1>Help/FAQ</h1>
+                    <h2>Here you can provide some helpful information or FAQs.</h2>
+
                 </div>
             </div>
         </>
