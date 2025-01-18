92dbe2b62cd5d5d08f9fca9f20101833c4e0e3f5
Jinny Yoo <hyejin.yoo@mail.utoronto.ca>
2025-01-18 15:53:25 -0500

    added button features to analysis.

--- src/Sidebar/sidebar.jsx
+++ src/Sidebar/sidebar.jsx
@@ -5,11 +5,22 @@ import {assets} from "../assets/assets.jsx";
 const Sidebar = () => {
 
     const [extended, setExtended] = useState(false)
+    const [showAnalysis, setShowAnalysis] = useState(false);
 
+    const toggleExtended = () => {
+        setExtended(prev=> !prev);
+        if (extended) {
+            setShowAnalysis(false);
+        }
+    }
+
+    const toggleAnalysis = () => {
+        setShowAnalysis(prev=> !prev);
+    }
     return (
         <div className="sidebar">
             <div className="top">
-                <img onClick={()=>setExtended(prev=> !prev)} className="menu" src={assets.menu_icon} alt=""/>
+                <img onClick={toggleExtended} className="menu" src={assets.menu_icon} alt=""/>
                 {extended
                     ? <div className="topics">
                         <img src={assets.topics_icon} alt=""/>
@@ -17,11 +28,21 @@ const Sidebar = () => {
                     </div>
                     : null}
                     {extended
-                        ? <div className="analysis">
+                        ? <div className="analysis"
+                        onClick={toggleAnalysis}>
                             <img src={assets.analysis_icon} alt=""/>
                             <p>Analysis</p>
                         </div>
                         : null}
+                {extended && showAnalysis &&(
+                    <div className="analysis">
+                        {["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"].map(option => (
+                            <button key={option} className={"analysisOptions"}>
+                                {option}
+                            </button>
+                        ))}
+                    </div>
+                )}
 
                     </div>
                     <div className="bottom">
