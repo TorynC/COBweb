a369dd6d7ce5f15b5293296fef00b29d4ca34000
SahilBas2005 <sahilbas2005@gmail.com>
2025-01-18 20:49:20 -0500

    Added Node Generation

--- src/ArticleNode.jsx
+++ src/ArticleNode.jsx
@@ -1,22 +1,19 @@
-import { useCallback } from 'react';
+import {useCallback, useContext} from 'react';
 import { Handle, Position } from '@xyflow/react';
 import "./ArticleNode.css";
+import {Context} from "./context/Context.jsx";
 
 const handleStyle = { left: 10 };
 
 function ArticleNode({ data, isConnectable }) {
+    const {setRoot, root} = useContext(Context);
     const onChange = useCallback((evt) => {
         console.log(evt.target.value);
     }, []);
 
     return (
         <div className="text-updater-node">
-            <Handle
-                position={Position.Top}
-                isConnectable={isConnectable}
-                type="target"
-            />
-            <div className="circle">
+            <div onClick={() => setRoot(data.value)} className="circle">
                 <p className="text">{data.value.title}</p>
             </div>
             <Handle
